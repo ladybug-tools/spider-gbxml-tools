4d0ceac372f0b58fda4a1c961b85ae822dae7674
Theo Armour <theo-armour@users.noreply.github.com>
2019-02-15 13:23:42 -0800

    Maevia 15.16 * Updates to Air Surface Type Editor

--- spider-gbxml-viewer/r15/js-gbxml/isaste-issues-air-surface-type-editor.js
+++ spider-gbxml-viewer/r15/js-gbxml/isaste-issues-air-surface-type-editor.js
@@ -3,7 +3,7 @@
 /* jshint esversion: 6 */
 
 
-const ISASTE = { "release": "R15.2", "date": "2019-02-14" };
+const ISASTE = { "release": "R15.3", "date": "2019-02-15" };
 
 ISASTE.description =
 	`
@@ -18,38 +18,83 @@ ISASTE.currentStatus =
 		<p>
 			${ ISASTE.description }
 		</p>
+
 		<p>
-			Proposed methodology:<br>
-			Create a Three.js 'ray' for each side of an Air surface type.
-			The ray follows the normal for that surface at the center point of the surface.
-			If one of the rays finds no intersections on one side then we know that the surface must be on the exterior.
-			If both rays find intersections then we know the surface must be inside the model.
-		</p>
-		<p>
-			<a href="https://github.com/ladybug-tools/spider-gbxml-tools/blob/master/spider-gbxml-viewer/r15/js-gbxml/ISASTE-issues-air-surface-tpe-editor.js: target="_blank" >
+			<a href="https://github.com/ladybug-tools/spider-gbxml-tools/blob/master/spider-gbxml-viewer/r15/js-gbxml/isaste-issues-air-surface-type-editor.js" target="_blank" >
 			Air Surface Type Editor source code
 			</a>
 		</p>
 
-		<p>
-			Issues
+		<details>
+
+			<summary>Usage</summary>
+
+			<p>
+				The current test case file for this project <a href="#https://rawgit.com/ladybug-tools/spider/master/gbxml-sample-files/zip/TypicalProblems-AirSurface.zip" >TypicalProblems-AirSurface.zip</a>
+				in the <a href="https://github.com/ladybug-tools/spider/tree/master/gbxml-sample-files" target="_blank">Spider gbXML sample files folder</a>.
+			</p>
+
+			<p>
+				The test case may be loaded by refreshing the web page or pressing F5.
+				Click the title in the menu to return to the default file.
+			</p>
+
+			<p>Usage is still at an early stage. </p>
+
+			<p>Select box and buttons should be working as expected.</p>
+
+			<p>
+				Identifying external Air surfaces is a work-in-progress.
+				<span class=highlight>The web page must be reloaded between each session.</span>
+				Click the numbered boxes in numerical order.
+				Various normals and sprites at intersection will be drawn. These are for testing while devloping and will not appear in final version.
+			</p>
+
+			<p>Editing surface types and saving your edits has not yet been started.</p>
+
+		</details>
+
+		<details>
+			<summary>Methodology</summary>
+
+			<p>See also Wikipedia: <a href="https://en.wikipedia.org/wiki/Point_in_polygon" target="_blank">Point in polygon</a></p>
+
+			Create a Three.js 'ray' for each side of an Air surface type.
+			The ray follows the normal for that surface at the center point of the surface.
+			If one of the rays finds no intersections on one side then we know that the surface must be on the exterior.
+			If both rays find intersections then we know the surface must be inside the model.
+
+		</details>
+
+
+		<details>
+			<summary>Issues</summary>
 			<ul>
-				<li>2019-02-13 ~ Only works once - must reload between each ron </li>
+				<li>2019-02-13 ~ what to show in select box? ID? Name?</li>
+				<li>2019-02-13 ~ Only works once - must reload between each run </li>
 			</ul>
 
-		</p>
+		</details>
+
 
 		<details>
 			<summary>Wish List / To Do</summary>
 			<ul>
+				<li>2019-02-15 ~ Easy access to show edges external vertical and/or horizontal suraces</li>
+				<li>2019-02-15 ~ Check normals for both sides of surfaces</li>
+				<li>2019-02-15 ~ Save changes to files</li>
+				<li>2019-02-15 ~ Identify separately vertical and horizontal surfaces</li>
+				<li>2019-02-15 ~ Show/hide identified incorrect surfaces</li>
 				<li>2019-02-14 ~ Reset variables when loading new file/li>
 				<li>2019-02-14 ~ Handle models with concavities</li>
 			</ul>
-		<details>
+		</details>
 
 		<details>
 			<summary>Change log</summary>
 			<ul>
+				<li>2019-02-15 ~ Add vertical surfaces display in red, horizontal surfaces in blue. (Not perfectly yet.)</li>
+				<li>2019-02-15 ~ Add text to popup help. Set default Air surface to display if none selected by user.</li>
 				<li>2019-02-14 ~ Add beginning of working check if Air surface types are on exterior of a model</li>
 				<li>2019-02-13 ~ Add find intersections between Air and other surfaces but not shade</li>
 				<li>2019-02-13 ~ Fixed using cursor keys updates select 3D at same time</li>
@@ -70,7 +115,7 @@ ISASTE.getMenuAirSurfaceTypeEditor = function() {
 
 	`<details id="ISASTEdetAirSurfaceTypeEditor" ontoggle=ISASTE.getAirSurfaceTypeEditorCheck(); >
 
-		<summary>Air Surface Type Editor<span id="ISASTEspnCount" ></span>
+		<summary class=highlight >Air Surface Type Editor<span id="ISASTEspnCount" ></span>
 		<a id=ISASTEsumHelp class=helpItem href="JavaScript:MNU.setPopupShowHide(ISASTEsumHelp,ISASTE.currentStatus);" >&nbsp; ? &nbsp;</a>
 
 		</summary>
@@ -90,11 +135,11 @@ ISASTE.getMenuAirSurfaceTypeEditor = function() {
 			</select>
 		</p>
 		<p>
-			Select multiple surfaces by pressing shift or control keys
+			Select multiple surfaces by pressing shift or control keys.
 		</p>
 		<p>
-			<button id=ISASTEbutViewSelected onclick=ISASTE.selectedSurfaceShowHide(this,ISASTEselAirSurfaceTypeEditor); title="" >
-				show/hide selected air surfaces
+			<button id=ISASTEbutViewSelected onclick=ISASTE.selectedSurfaceShowHide(this,ISASTEselAirSurfaceTypeEditor); title="If none selected, first is taken as default" >
+				show/hide currently selected air surfaces
 			</button>
 		</p>
 
@@ -120,241 +165,17 @@ ISASTE.getMenuAirSurfaceTypeEditor = function() {
 		<p>
 			Update surface(s) type to:
 			<button onclick=ISASTE.setNewSurfaceType(this); style="background-color:#aaa;" >ExposedFloor</button><br>
-			<button onclick=ISASTE.setNewSurfaceType(this) style="background-color:#ffb400;" >ExteriorWall</button><br>
-			<button onclick=ISASTE.setNewSurfaceType(this) style="background-color:#800000;" >Roof</button><br>
-			<button onclick=ISASTE.setNewSurfaceType(this) style="background-color:#ffce9d;" >Shade</button><br>
-			<button onclick=ISASTE.setNewSurfaceType(this) style="background-color:#804000;" >SlabOnGrade</button><br>
-			<button onclick=ISASTE.setNewSurfaceType(this) style="background-color:#a55200;" >UndergroundWall</button><br>
-			<button onclick=ISASTE.setNewSurfaceType(this) style="background-color:#804000;" >UndergroundSlab</button><br>
+			<button onclick=ISASTE.setNewSurfaceType(this); style="background-color:#ffb400;" >ExteriorWall</button><br>
+			<button onclick=ISASTE.setNewSurfaceType(this); style="background-color:#800000;" >Roof</button><br>
+			<button onclick=ISASTE.setNewSurfaceType(this); style="background-color:#ffce9d;" >Shade</button><br>
+			<button onclick=ISASTE.setNewSurfaceType(this); style="background-color:#804000;" >SlabOnGrade</button><br>
+			<button onclick=ISASTE.setNewSurfaceType(this); style="background-color:#a55200;" >UndergroundWall</button><br>
+			<button onclick=ISASTE.setNewSurfaceType(this); style="background-color:#804000;" >UndergroundSlab</button><br>
 		</p>
 
 	</details>`;
 
 	return htm;
-
-};
-
-
-
-ISASTE.addNormals = function( button ) {
-
-	button.classList.toggle( "active" );
-
-	THRU.groundHelper.visible = !button.classList.contains( 'active' );
-
-	if ( button.classList.contains( 'active' ) ) {
-
-		GBX.surfaceGroup.children.forEach( mesh => mesh.visible = false );
-
-		ISASTE.surfaceAirIndices.forEach( surfaceId => GBX.surfaceGroup.children[ surfaceId ].visible = true );
-
-	}
-
-	ISASTE.toggleSurfaceNormals();
-
-};
-
-
-
-ISASTE.toggleSurfaceNormals = function() {
-
-	let material = new THREE.MeshNormalMaterial();
-
-	const types = [ 'BoxBufferGeometry', 'BufferGeometry', 'ConeBufferGeometry', 'CylinderBufferGeometry',
-		'ShapeBufferGeometry', 'SphereBufferGeometry' ];
-
-	if ( ISASTE.airNormalsFaces === undefined ) {
-
-		ISASTE.airNormalsFaces = new THREE.Group();
-
-		THR.scene.traverse( function ( child ) {
-
-			if ( child instanceof THREE.Mesh && child.visible ) {
-
-				if ( child.geometry.type === 'Geometry' ) {
-
-					child.geometry.computeFaceNormals();
-
-					const helperNormalsFace = new THREE.FaceNormalsHelper( child, 2, 0xff00ff, 3 );
-					ISASTE.airNormalsFaces.add( helperNormalsFace );
-					ISASTE.airNormalsFaces.visible = false;
-					console.log( 'helperNormalsFace', helperNormalsFace );
-
-				} else if ( types.includes( child.geometry.type ) === true ) {
-
-					//console.log( 'child', child.position, child.rotation );
-
-					const geometry = new THREE.Geometry();
-					const geo = geometry.fromBufferGeometry( child.geometry );
-					const mesh = new THREE.Mesh( geo, material );
-					mesh.rotation.copy( child.rotation );
-					mesh.position.copy( child.position );
-					const helperNormalsFace = new THREE.FaceNormalsHelper( mesh, 0.05 * THRU.radius, 0xff00ff, 3 );
-
-					ISASTE.airNormalsFaces.add( helperNormalsFace );
-					ISASTE.airNormalsFaces.visible = false;
-
-				} else {
-
-					//console.log( 'child.geometry.type', child.geometry.type );
-
-				}
-
-			}
-
-		} );
-
-		ISASTE.airNormalsFaces.name = 'airNormalsFaces';
-		THR.scene.add( ISASTE.airNormalsFaces );
-		ISASTE.airNormalsFaces.visible = false;
-
-	}
-
-	ISASTE.airNormalsFaces.visible = !ISASTE.airNormalsFaces.visible;
-
-};
-
-
-
-ISASTE.setTargetSurfacesVisible = function( button ) {
-
-	button.classList.toggle( "active" );
-
-	ISASTE.surfaceNotShadeIndices = [];
-
-	const surfaces = GBX.surfaces;
-
-	// refactor to a reduce??
-	for ( let i = 0; i < surfaces.length; i++ ) {
-
-		const surface = surfaces[ i ];
-		//console.log( 'surface', surface );
-		const surfaceMatch = surface.match( /surfaceType="Shade"/ );
-		//console.log( 'surfaceMatch', surfaceMatch );
-
-		if ( !surfaceMatch ) {
-
-			ISASTE.surfaceNotShadeIndices.push( i );
-
-		}
-
-	}
-
-	if ( button.classList.contains( 'active' ) ) {
-
-		GBX.surfaceGroup.children.forEach( mesh => mesh.visible = false );
-
-		ISASTE.surfaceNotShadeIndices.forEach( surfaceId => GBX.surfaceGroup.children[ surfaceId ].visible = true );
-
-	}
-
-};
-
-
-
-ISASTE.castRaysGetIntersections = function( button ) {
-
-	button.classList.toggle( "active" );
-
-	let fixes = 0;
-
-	const normals = ISASTE.airNormalsFaces;
-	//console.log( 'normals', normals );
-
-	for ( let normal of normals.children ) {
-
-		const coordinates = normal.geometry.attributes.position.array;
-		//console.log( 'coordinates', coordinates );
-
-		for ( let i  = 0; i < coordinates.length;) {
-
-			const geometry = new THREE.BoxBufferGeometry( 0.2, 0.2, 0.2 );
-			const material = new THREE.MeshNormalMaterial();
-			const mesh = new THREE.Mesh( geometry, material );
-			mesh.position.set( coordinates[ i++ ], coordinates[ i++ ],coordinates[ i++ ] );
-			//THR.scene.add( mesh );
-
-		}
-
-	}
-
-	const v = ( x, y, z ) => new THREE.Vector3( x, y, z );
-
-	for ( let normal of normals.children ) {
-
-		const coordinates = normal.geometry.attributes.position.array;
-		//console.log( 'coordinates', coordinates );
-
-		for ( let i = 0; i < coordinates.length; ) {
-
-			const vertex1 = v( coordinates[ i++ ], coordinates[ i++ ], coordinates[ i++ ] );
-			const vertex2 = v( coordinates[ i++ ], coordinates[ i++ ],coordinates[ i++ ] );
-			//console.log( 'vertex1', vertex1 );
-
-			const direction = vertex2.clone().sub( vertex1 ).normalize();
-			//console.log( 'direction', direction );
-
-			fixes += findIntersections( GBX.surfaceGroup.children, vertex1, direction );
-
-		}
-
-	}
-
-	ISASTEfixes.innerHTML = `Air surfaces on exterior identified: ${ fixes }`;
-
-};
-
-
-
-function findIntersections( objs, origin, direction ) {
-
-	let count = 0;
-	const spriteMaterial = new THREE.SpriteMaterial( { color: 'magenta' } );
-
-	const raycaster = new THREE.Raycaster();
-	near = 0.001 * THRU.radius;
-	raycaster.set( origin, direction, near, THRU.radius ); // has to be the correct vertex order
-
-	const intersects = raycaster.intersectObjects( objs );
-	//console.log( 'intersects', intersects );
-
-	for ( let intersect of intersects ) {
-
-		if ( intersect.distance > 0 ) {
-
-			const sprite = new THREE.Sprite( spriteMaterial );
-			s = 0.005 * THRU.radius;
-			sprite.scale.set( s, s, s,);
-			sprite.position.copy( intersect.point );
-
-			THR.scene.add( sprite );
-
-		}
-
-
-	}
-
-	if ( intersects.length === 1 ) {
-
-		//console.log( 'intersect.object', intersects[ 0 ].object );
-
-		intersects[ 0 ].object.material = new THREE.MeshNormalMaterial( { side: 2 });
-
-		intersects[ 0 ].object.material.needsUpdate = true;
-
-		count++;
-
-	}
-
-	return count;
-
-}
-
-
-
-ISASTE.setNewSurfaceType = function( that ) {
-
-	alert( "coming soon!" );
 
 };
 
@@ -411,6 +232,7 @@ ISASTE.getAirSurfaceTypeEditorCheck = function() {
 
 
 
+
 ISASTE.setAirSurfaceTypeEditorShowHide = function( button, surfaceArray ) {
 
 	button.classList.toggle( "active" );
@@ -451,6 +273,12 @@ ISASTE.selectedSurfaceShowHide = function( button, select ) {
 
 	THR.controls.enableKeys = false;
 
+	if ( ISASTEselAirSurfaceTypeEditor.selectedOptions.length === 0 ) {
+
+		ISASTEselAirSurfaceTypeEditor.selectedIndex = 0
+
+	}
+
 	options = Array.from( select.selectedOptions );
 	//console.log( 'options', options );
 
@@ -476,4 +304,254 @@ ISASTE.selectedSurfaceShowHide = function( button, select ) {
 
 
 }
+
+
+
+//////////
+
+
+ISASTE.addNormals = function( button ) {
+
+	button.classList.toggle( "active" );
+
+	THRU.groundHelper.visible = !button.classList.contains( 'active' );
+
+	if ( button.classList.contains( 'active' ) ) {
+
+		GBX.surfaceGroup.children.forEach( mesh => mesh.visible = false );
+
+		ISASTE.surfaceAirIndices.forEach( surfaceId => GBX.surfaceGroup.children[ surfaceId ].visible = true );
+
+	}
+
+	ISASTE.toggleSurfaceNormals();
+
+};
+
+
+
+ISASTE.toggleSurfaceNormals = function() {
+
+	let material = new THREE.MeshNormalMaterial();
+
+	const types = [ 'BoxBufferGeometry', 'BufferGeometry', 'ConeBufferGeometry', 'CylinderBufferGeometry',
+		'ShapeBufferGeometry', 'SphereBufferGeometry' ];
+
+	//if ( ISASTE.airNormalsFaces === undefined ) {
+
+		ISASTE.airNormalsFaces = new THREE.Group();
+
+		THR.scene.traverse( function ( child ) {
+
+			if ( child instanceof THREE.Mesh && child.visible ) {
+
+				if ( child.geometry.type === 'Geometry' ) {
+
+					child.geometry.computeFaceNormals();
+
+					const helperNormalsFace = new THREE.FaceNormalsHelper( child, 2, 0xff00ff, 3 );
+					ISASTE.airNormalsFaces.add( helperNormalsFace );
+					ISASTE.airNormalsFaces.visible = false;
+					console.log( 'helperNormalsFace', helperNormalsFace );
+
+				} else if ( types.includes( child.geometry.type ) === true ) {
+
+					//console.log( 'child', child.position, child.rotation );
+
+					const geometry = new THREE.Geometry();
+					const geo = geometry.fromBufferGeometry( child.geometry );
+					const mesh = new THREE.Mesh( geo, material );
+					mesh.rotation.copy( child.rotation );
+					mesh.position.copy( child.position );
+					const helperNormalsFace = new THREE.FaceNormalsHelper( mesh, 0.05 * THRU.radius, 0xff00ff, 3 );
+
+					ISASTE.airNormalsFaces.add( helperNormalsFace );
+					ISASTE.airNormalsFaces.visible = false;
+
+				} else {
+
+					//console.log( 'child.geometry.type', child.geometry.type );
+
+				}
+
+			}
+
+		} );
+
+		ISASTE.airNormalsFaces.name = 'airNormalsFaces';
+		THR.scene.add( ISASTE.airNormalsFaces );
+		ISASTE.airNormalsFaces.visible = false;
+
+	//}
+
+	ISASTE.airNormalsFaces.visible = !ISASTE.airNormalsFaces.visible;
+
+};
+
+
+
+ISASTE.setTargetSurfacesVisible = function( button ) {
+
+	button.classList.toggle( "active" );
+
+	ISASTE.surfaceNotShadeIndices = [];
+
+	const surfaces = GBX.surfaces;
+
+	// refactor to a reduce??
+	for ( let i = 0; i < surfaces.length; i++ ) {
+
+		const surface = surfaces[ i ];
+		//console.log( 'surface', surface );
+		const surfaceMatch = surface.match( /surfaceType="Shade"/ );
+		//console.log( 'surfaceMatch', surfaceMatch );
+
+		if ( !surfaceMatch ) {
+
+			ISASTE.surfaceNotShadeIndices.push( i );
+
+		}
+
+	}
+
+	if ( button.classList.contains( 'active' ) ) {
+
+		GBX.surfaceGroup.children.forEach( mesh => mesh.visible = false );
+
+		ISASTE.surfaceNotShadeIndices.forEach( surfaceId => GBX.surfaceGroup.children[ surfaceId ].visible = true );
+
+	}
+
+};
+
+
+
+ISASTE.castRaysGetIntersections = function( button ) {
+
+	button.classList.toggle( "active" );
+
+	ISASTE.airSurfacesOnExterior = [];
+
+	let fixes = 0;
+
+	const normals = ISASTE.airNormalsFaces;
+	//console.log( 'normals', normals );
+
+	for ( let normal of normals.children ) {
+
+		const coordinates = normal.geometry.attributes.position.array;
+		//console.log( 'coordinates', coordinates );
+
+		for ( let i  = 0; i < coordinates.length;) {
+
+			const geometry = new THREE.BoxBufferGeometry( 0.2, 0.2, 0.2 );
+			const material = new THREE.MeshNormalMaterial();
+			const mesh = new THREE.Mesh( geometry, material );
+			mesh.position.set( coordinates[ i++ ], coordinates[ i++ ],coordinates[ i++ ] );
+			//THR.scene.add( mesh );
+
+		}
+
+	}
+
+	const v = ( x, y, z ) => new THREE.Vector3( x, y, z );
+
+	for ( let normal of normals.children ) {
+
+		const coordinates = normal.geometry.attributes.position.array;
+		//console.log( 'coordinates', coordinates );
+
+		for ( let i = 0; i < coordinates.length; ) {
+
+			const vertex1 = v( coordinates[ i++ ], coordinates[ i++ ], coordinates[ i++ ] );
+			const vertex2 = v( coordinates[ i++ ], coordinates[ i++ ],coordinates[ i++ ] );
+			//console.log( 'vertex1', vertex1 );
+
+			const direction = vertex2.clone().sub( vertex1 ).normalize();
+			//console.log( 'direction', direction );
+
+			fixes += ISASTE.findIntersections( GBX.surfaceGroup.children, vertex1, direction );
+
+		}
+
+	}
+
+	ISASTEfixes.innerHTML = `Air surfaces on exterior identified: ${ fixes }`;
+
+	GBX.surfaceGroup.children.forEach( mesh => mesh.visible = false );
+
+	ISASTE.surfaceAirIndices.forEach( surfaceId => GBX.surfaceGroup.children[ surfaceId ].visible = true );
+
+	ISASTE.airNormalsFaces.visible = false;
+
+};
+
+
+
+ISASTE.findIntersections = function( objs, origin, direction ) {
+
+
+	let count = 0;
+	const spriteMaterial = new THREE.SpriteMaterial( { color: 'magenta' } );
+
+	const raycaster = new THREE.Raycaster();
+	near = 0.001 * THRU.radius;
+	raycaster.set( origin, direction, near, THRU.radius ); // has to be the correct vertex order
+
+	const intersects = raycaster.intersectObjects( objs );
+	//console.log( 'intersects', intersects );
+
+	/*
+	ISASTE.sprites = new THREE.Group();
+	const s = 0.005 * THRU.radius;
+
+	for ( let intersect of intersects ) {
+
+		if ( intersect.distance > 0 ) {
+
+			const sprite = new THREE.Sprite( spriteMaterial );
+			sprite.scale.set( s, s, s,);
+			sprite.position.copy( intersect.point );
+
+			ISASTE.sprites.add( sprite );
+
+		}
+
+	}
+
+	THR.scene.add( ISASTE.sprites );
+
+	*/
+
+	if ( intersects.length === 1 ) {
+
+		//console.log( 'intersect.object', intersects[ 0 ].object );
+
+		mesh = intersects[ 0 ].object;
+
+		color = ( Math.abs( mesh.geometry.attributes.normal.array[ 2 ] ) === 1 ) ? 'blue' : 'red';
+
+		mesh.material = new THREE.MeshBasicMaterial( { color: color, side: 2 });
+
+		mesh.material.needsUpdate = true;
+
+		ISASTE.airSurfacesOnExterior.push( mesh.userData.index )
+
+		count++;
+
+	}
+
+	return count;
+
+}
+
+
+
+ISASTE.setNewSurfaceType = function( that ) {
+
+	alert( "coming soon!" );
+
+};
+
+
 
