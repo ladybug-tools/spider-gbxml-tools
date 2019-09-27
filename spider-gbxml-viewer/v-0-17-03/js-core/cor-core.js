f426fb9b9a08cd0c293d564079118810f262ba85
Theo Armour <theo-armour@users.noreply.github.com>
2019-09-26 18:38:38 -0700

    popup fixes

--- spider-gbxml-viewer/v-0-17-03/js-core/cor-core.js
+++ spider-gbxml-viewer/v-0-17-03/js-core/cor-core.js
@@ -13,7 +13,7 @@ const COR = {
 	}
 };
 
-THM.cssBasic = "https://pushme-pullyou.github.io/tootoo14/js-14-07/style.css";
+THM.cssBasic = "style.css";
 
 
 POP.license = "https://www.ladybug.tools/spider/pages/license.md";
